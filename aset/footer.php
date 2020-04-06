74d2562a26a75c37ce20c352af9f1f35db5c7866
Yeremia395 <53548038+Yeremia395@users.noreply.github.com>
2020-04-06 18:34:11 +0700

    first commit

---
+++ aset/footer.php
@@ -0,0 +1,4 @@
+<script src="http://localhost/php/challenge/aset/jquery.js"></script>
+<script src="http://localhost/php/challenge/aset/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ siswa/koneksi.php
@@ -0,0 +1,11 @@
+<?php
+    $koneksi = mysqli_connect("localhost", "root", "", "db_challange");
+
+    //cek koneksi
+
+    if(!$koneksi){
+        echo "";
+    }else{
+        echo "";
+    }
+?>
